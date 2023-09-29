fba7847b5db4afffe2857086510de870c6b0190b
Adrian <hodurbalder@gmail.com>
2023-09-28 20:35:37 -0600

    remove bucket

--- src/config.js
+++ src/config.js
@@ -8,18 +8,4 @@ module.exports = {
     salt: process.env.SALT,
     cron: process.env.CRON === 'true',
     app: process.env.APP,
-
-    bucket: {
-        name: process.env.GOOGLE_STORAGE_NAME,
-        type: process.env.GOOGLE_STORAGE_TYPE,
-        project_id: process.env.GOOGLE_STORAGE_PROJECT_ID,
-        private_key_id: process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_STORAGE_PRIVATE_KEY,
-        client_email: process.env.GOOGLE_STORAGE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_STORAGE_CLIENT_ID,
-        auth_uri: process.env.GOOGLE_STORAGE_AUTH_URI,
-        token_uri: process.env.GOOGLE_STORAGE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.GOOGLE_STORAGE_CLIENT_X509_CERT_URL,
-    },
 }
--- src/services.js
+++ src/services.js
@@ -1,4 +1,3 @@
 module.exports.Users = require('./users/users.service')
 module.exports.Sessions = require('./sessions/sessions.service')
-module.exports.Buckets = require('./buckets/buckets.service')
 module.exports.Games = require('./games/games.service')
